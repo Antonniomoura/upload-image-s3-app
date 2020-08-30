168e3d17db7b53e71f49aac8d3e4d9d0699aebf6
Antonio Moura <antonniogt@gmail.com>
2020-08-30 13:38:12 -0300

    djustment

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.NODE_ENV === 'development'
-        ? 'http://localhost:3001/'
-        : process.env.URL_PROD,
+    baseURL: 'http://54.146.159.85/',
     headers: {
         code: localStorage.getItem('code'),
     }
